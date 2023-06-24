29123276a29583571ecbb24790132a735252b47e
Divyansh Tiwari <divyansh.tiwari007@gmail.com>
2023-06-25 03:20:53 +0530

    removed as from the import statement to be somewhat consistent with the book

--- guessTheNumber.py
+++ guessTheNumber.py
@@ -1,6 +1,6 @@
 #This is a guess the number game.
-import random as rnd
-secretNumber = rnd.randint(1,20)
+import random
+secretNumber = random.randint(1,20)
 print('I am thinking of a number between 1 and 20.')
 
 #Ask the player to guess 6 times.

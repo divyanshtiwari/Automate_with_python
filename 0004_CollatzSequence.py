ab2834450142b471f0baf00e846b70501104748b
Divyansh Tiwari <divyansh.tiwari007@gmail.com>
2023-07-03 15:43:55 +0530

    added input validator with try and except statements

--- 0004_CollatzSequence.py
+++ 0004_CollatzSequence.py
@@ -1,10 +1,14 @@
+import sys
 def collatz(number):
     if number % 2 ==0 :
         return number //2
     else :
         return 3*number + 1
-    
-number = int(input("Enter number to start Collatz Sequence from : "))
-while number != 1 :
-    number = collatz(number)
-    print(number)
+try:
+    number = int(input("Enter number to start Collatz Sequence from : "))
+    while number != 1 :
+        number = collatz(number)
+        print(number)
+except ValueError :
+    print("You have not entred a valid input, the input should be a valid integer.")
+    sys.exit()

d446e28db537c420e4f88de8cd9ad5d73b3c04dd
Divyansh Tiwari <divyansh.tiwari007@gmail.com>
2023-08-13 11:30:15 +0530

    using relative path to the interpreter

--- 0010_mclip.py
+++ 0010_mclip.py
@@ -1,4 +1,5 @@
-#! F:\My_Tech_Journey_2023\python\Automate_with_python\Scripts\python
+#! .\Scripts\python
+# note : previously i was using the entire path, but now i am using the relative path
 # # The above first line is a shebang line which starts with a Hash and exclimation mark. This line define the location of the interpreter to be used for the execution of this program.
 # Note1 : shebang line should be the first line in a script, otherwise it will not work as expected. 
 # Note2 : defining a shebang with specific path will endup loosing potability for this shebang has provided virtual command, so that the portability can be retained.
